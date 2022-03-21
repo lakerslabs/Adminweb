3a571acce5bb0c1ecc6a14988e708c32efb60782
bergaeduardo <bergaeduardo@gmail.com>
2022-03-21 16:56:10 +0100

    Configuracion para puesta en produccion

--- core/production.py
+++ core/production.py
@@ -5,7 +5,8 @@ from decouple import config
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['192.168.1.127', 'localhost']
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -17,13 +18,24 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'BD_Pruebas',
+#         'USER': 'openpg',
+#         'PASSWORD': 'openpgpwd',
+#         'HOST': '127.0.0.1',
+#         'DATABASE_PORT': '5232',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'BD_Pruebas',
-        'USER': 'openpg',
-        'PASSWORD': 'openpgpwd',
-        'HOST': '127.0.0.1',
-        'DATABASE_PORT': '5232',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'DATABASE_PORT': '5432',
     }
 }
--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['192.168.1.127','localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
 
 
