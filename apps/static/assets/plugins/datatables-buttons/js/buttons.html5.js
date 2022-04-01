28e68d1056ff1367410fa0b56933788c196542f9
bergaeduardo <bergaeduardo@gmail.com>
2022-04-01 16:42:02 -0300

    reemplazo de carpeta plugins, block Js y yemplate de stock

--- apps/static/assets/plugins/datatables-buttons/js/buttons.html5.js
+++ apps/static/assets/plugins/datatables-buttons/js/buttons.html5.js
@@ -962,7 +962,7 @@ DataTable.ext.buttons.csvHtml5 = {
 		}
 
 		if ( config.bom ) {
-			output = '\ufeff' + output;
+			output = String.fromCharCode(0xFEFF) + output;
 		}
 
 		_saveAs(
@@ -1063,7 +1063,9 @@ DataTable.ext.buttons.excelHtml5 = {
 				}
 
 				var originalContent = row[i];
-				row[i] = $.trim( row[i] );
+				row[i] = typeof row[i].trim === 'function'
+					? row[i].trim()
+					: row[i];
 
 				// Special number formatting options
 				for ( var j=0, jen=_excelSpecials.length ; j<jen ; j++ ) {
