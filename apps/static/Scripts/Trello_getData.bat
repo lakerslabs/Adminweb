99741c54fca4be2c4b00479fd6862af7da64331a
bergaeduardo <bergaeduardo@gmail.com>
2024-10-01 15:45:10 -0300

    Add archivo bat

---
+++ apps/static/Scripts/Trello_getData.bat
@@ -0,0 +1,5 @@
+@echo 
+cd C:\Users\eduardo.berga\Desktop\Proyectos_Django\Intranet\env\Scripts
+call activate.bat
+cd C:\Users\eduardo.berga\Desktop\Proyectos_Django\Intranet\Adminweb\apps\static\Scripts
+python.exe C:\Users\eduardo.berga\Desktop\Proyectos_Django\Intranet\Adminweb\apps\static\Scripts\getData_Trello.py
