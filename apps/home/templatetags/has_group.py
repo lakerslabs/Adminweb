61442dc1382d57a35e09bfd7ebbae733b68c4371
bergaeduardo <bergaeduardo@gmail.com>
2024-12-24 11:20:55 -0300

    Correccioines y mejoras 24-12

--- apps/home/templatetags/has_group.py
+++ apps/home/templatetags/has_group.py
@@ -13,3 +13,18 @@ def group_id(user, idgroup):
     grupo = user.groups.filter(id__exact=idgroup).exists() 
     # print(grupo)
     return grupo
+
+@register.filter(name='has_any_group')
+def has_any_group(user, groups):
+    """Verifica si el usuario pertenece a alguno de los grupos especificados."""
+    if not user or not user.is_authenticated:
+        return False
+    group_list = groups.split(',')
+    return user.groups.filter(name__in=group_list).exists()
+
+@register.filter(name='get_user_groups')
+def get_user_groups(user):
+    """Obtiene una lista de los nombres de los grupos a los que pertenece el usuario."""
+    if not user or not user.is_authenticated:
+        return []
+    return [group.name for group in user.groups.all()]
